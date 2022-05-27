bfc376ba1ec344653b4d8db954e08d51e2be8bb7
uzzal71 <uzzalroy.acm@gmail.com>
2022-05-27 22:53:39 +0600

    Create DesignationModel

--- src/models/designationModel.js
+++ src/models/designationModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-export const DepartmentSchema = new Schema({
+export const DesignationSchema = new Schema({
     name: {
         type: String,
         required: true
