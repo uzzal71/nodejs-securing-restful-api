60b76becc3bf31fb9e0fb731f3492dd38f990ba6
uzzal71 <uzzalroy.acm@gmail.com>
2022-05-29 23:54:17 +0600

    changed provident fund

---
+++ src/models/providentFundModel.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const ProvidentFundSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    }
+});
