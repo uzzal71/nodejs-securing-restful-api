c11e52a364b6fd8d6b4f7c11f4e864a2b11b04a9
uzzal71 <uzzalroy.acm@gmail.com>
2022-05-29 23:55:44 +0600

    provident fund add create date

--- src/models/providentFundModel.js
+++ src/models/providentFundModel.js
@@ -11,4 +11,8 @@ export const ProvidentFundSchema = new Schema({
         type: Double,
         required: true
     },
+    created_date: {
+        type: Date,
+        default: Date.now 
+     }
 });
