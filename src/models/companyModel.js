bba56d3c615856996cc9f6dd8cc8d0554845be51
uzzal71 <uzzalroy.acm@gmail.com>
2022-05-29 23:56:45 +0600

    company model changed

---
+++ src/models/companyModel.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const CompanySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    created_date: {
+        type: Date,
+        default: Date.now 
+     }
+});
